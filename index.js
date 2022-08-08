7d04821c1cbda42011904c4d029bf08c5d484e6c
Alabi Khalid <alabikhalid.12@gmail.com>
2022-08-08 14:03:36 +0100

    updated readMe

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const submitBtn = document.querySelector(".submit");
 const rateClick = rate.forEach(function (ele, i, arr) {
   ele.addEventListener("click", function () {
     ele.style.backgroundColor = "hsl(216, 12%, 54%)";
+    ele.style.color = " hsl(0, 0%, 100%)";
+
     submitBtn.addEventListener("click", function () {
       document.querySelector(".container__rating").style.display = "none";
       document.querySelector(".container__thank-you").style.display = "block";
